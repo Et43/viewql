bf3d1e6dc9eb6bd63e511b2ab599fdd059ddd1d9
Ethan <ethanhavinga@gmail.com>
2025-02-15 22:22:00 +0200

    Spring-boilerplate

---
+++ src/main/java/com/mwrcybersec/viewql/viewql/ViewqlApplication.java
@@ -0,0 +1,13 @@
+package com.mwrcybersec.viewql.viewql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewqlApplication.class, args);
+	}
+
+}
