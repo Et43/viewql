78f5c86242230d44fbdbe12cc2f8060866adb157
Ethan <49207751+Et43@users.noreply.github.com>
2025-02-18 21:48:26 +0200

    Update CodeQLSetupTest.java

--- src/test/java/com/mwrcybersec/viewql/CodeQLSetupTest.java
+++ src/test/java/com/mwrcybersec/viewql/CodeQLSetupTest.java
@@ -8,15 +8,13 @@ public class CodeQLSetupTest {
 
     @Test
     public void testCodeQLSetup() {
-        String cliPath = "C:/Users/ethan/Downloads/codeql-win64/codeql/codeql.exe";
-        String extractorsPath = "C:/Users/ethan/Downloads/codeql-win64/codeql/java/tools";
+        String cliPath = "/opt/codeql/codeql";
+        String extractorsPath = "/opt/codeql/java/tools";
         
         assertDoesNotThrow(() -> {
             CodeQLEntryPoint.setupCodeQLEnvironment(cliPath, extractorsPath, "java");
             
             assertNotNull(System.getProperty("codeql.cli.path"));
-            assertNotNull(System.getProperty("codeql.extractors.path"));
-            assertNotNull(System.getProperty("CODEQL_EXTRACTOR_JAVA_ROOT"));
         });
     }
 }
